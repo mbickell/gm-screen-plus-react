1751cda8b64e739789f590d24daff0560e16e083
Matthew Bickell <matthew.bickell@Nology.local>
2019-11-10 15:33:12 +0000

    react setup

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import styles from "./App.module.scss";
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default App;
